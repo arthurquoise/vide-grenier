fd76608bedc9611846079e4bb30e65600d79bcf7
Nicolas <v.nicolasvanhuysse@free.fr>
2022-06-15 15:12:59 +0200

    feature contact

--- App/Controllers/Product.php
+++ App/Controllers/Product.php
@@ -49,6 +49,7 @@ class Product extends \Core\Controller
     public function showAction()
     {
         $id = $this->route_params['id'];
+        $f = $_POST;
 
         try {
             Articles::addOneView($id);
@@ -58,9 +59,48 @@ class Product extends \Core\Controller
             var_dump($e);
         }
 
+        if(isset($_POST['sendMail']))
+        {
+            $this->sendMailAction($f);
+        }
+
         View::renderTemplate('Product/Show.html', [
             'article' => $article[0],
             'suggestions' => $suggestions
         ]);
+    }
+
+    private function sendMailAction($data)
+    {   
+        if(isset($data['email']) and isset($data['message']))
+        {   
+
+            $destinataire = 'vanhuysse1@gmail.com';
+            $email = htmlentities($data['email']);
+            if(preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i',str_replace('&amp;','&',$email)))
+            {
+                $sujet = 'Contact';
+                $message = stripslashes($data['message']);
+                $headers = "From: <".$email.">\n";
+                $headers .= "Reply-To: ".$email."\n";
+                $headers .= "Content-Type: text/plain; charset=\"iso-8859-1\"";
+                if(mail($destinataire,$sujet,$message,$headers))
+                    {
+                    echo "<p class='message'>Votre message a bien &eacute;t&eacute; envoy&eacute;.</p>";
+                    }
+                else
+                    {
+                    echo "<p class='message'>Une erreur c'est produite lors de l'envois du message.</p>";
+                    }
+            }
+            else
+            {
+                echo "<p class='message'>L'email que vous avez entr&eacute; est invalide.</p>";
+            }
+        }
+        else
+        {
+
+        }
     }
 }
